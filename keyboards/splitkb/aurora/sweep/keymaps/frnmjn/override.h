6aeb187e79f8b8a07fb408207ab4261deac015a2
Emilio Junior Francischetti <ejfrancischetti@gmail.com>
2025-05-15 21:56:49 +0200

    wip

--- keyboards/splitkb/aurora/sweep/keymaps/frnmjn/override.h
+++ keyboards/splitkb/aurora/sweep/keymaps/frnmjn/override.h
@@ -1,5 +1,5 @@
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-const key_override_t alt_left            = ko_make_basic(MOD_MASK_ALT, KC_L, A(KC_LEFT));
+const key_override_t alt_left            = ko_make_basic(MOD_MASK_ALT, _HR_8, A(KC_LEFT));
 const key_override_t alt_right           = ko_make_basic(MOD_MASK_ALT, KC_H, A(KC_RIGHT));
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
